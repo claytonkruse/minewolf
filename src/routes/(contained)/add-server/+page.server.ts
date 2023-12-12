b05a66febf90f891a67ef8a1c4fef0e54bbc089e
Hiemswolf <claytonkruse08@gmail.com>
2023-12-11 21:55:41 -0600

    zod schema work and prisma

--- src/routes/(contained)/add-server/+page.server.ts
+++ src/routes/(contained)/add-server/+page.server.ts
@@ -10,13 +10,20 @@ export const load: PageServerLoad = async ({ locals }) => {
 	return {};
 };
 
-import { MIN_SERVER_NAME_LENGTH, MAX_SERVER_NAME_LENGTH } from '$lib/server/config';
+import {
+	MIN_SERVER_NAME_LENGTH,
+	MAX_SERVER_NAME_LENGTH,
+	MAX_SERVER_SLOGAN_LENGTH,
+	MAX_SERVER_DESCRIPTION_LENGTH,
+	MAX_SERVER_WEBSITE_URL_LENGTH
+} from '$lib/server/config';
 import find_illegal_chars from '$lib/utils/find_illegal_chars';
 
 const schema = z.object({
 	// username: z.string({ required_error: 'You must enter a username.' }).trim().min(1),
 	// password: z.string().trim().optional(),
-
+	ip: z.string().trim().min(3, { message: 'A server IP or hostname is required.' }).max(40),
+	port: z.number().int().nonnegative().lte(65535),
 	name: z
 		.string({ required_error: 'You must enter a server name.' })
 		.trim()
@@ -26,20 +33,37 @@ const schema = z.object({
 			(name) => !find_illegal_chars(name, 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ'),
 			{ message: 'Name may only contain letters.' }
 		),
-	ip: z.string().trim().min(3, { message: 'A server IP or hostname is required.' }).max(40),
-	port: z.number().int().nonnegative().lte(65535)
+	slogan: z.string().trim().max(MAX_SERVER_SLOGAN_LENGTH),
+	description: z.string().trim().max(MAX_SERVER_DESCRIPTION_LENGTH),
+	website: z.string().trim().max(MAX_SERVER_WEBSITE_URL_LENGTH).url(),
+	video: z.string().trim().max(MAX_SERVER_WEBSITE_URL_LENGTH).url(),
+	dynmap: z.string().trim().max(MAX_SERVER_WEBSITE_URL_LENGTH).url(),
+	discord: z.string().trim().max(MAX_SERVER_WEBSITE_URL_LENGTH).url(),
+	edition: z
+		.string()
+		.trim()
+		.toLowerCase()
+		.refine((val) => ['java', 'bedrock', 'seperate', 'crossplay'].includes(val)),
+	whitelisted: z.boolean()
 });
 
 export const actions = {
 	default: async ({ locals, request }) => {
 		const session = await locals.auth.validate();
 		if (!session) throw error(401, 'Client must be authenticated to add a server.');
-		const { name, ip, port } = Object.fromEntries(await request.formData());
+
+		const formData = await request.formData();
+		const parsed = await schema.safeParseAsync(formData);
+		if (!parsed.success) {
+			const { fieldErrors: errors } = parsed.error.flatten();
+			return { data: formData, errors };
+		}
+
 		prisma.server.create({
 			data: {
-				name,
-				ip,
-				port
+				user_id: session.user.userId,
+				version: 'unknown',
+				...parsed.data
 			}
 		});
 	}
